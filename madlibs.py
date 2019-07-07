7fdb53ebae5681fa49711b3fd509724c7c0df0cd
jesse collier <jesse.collier97@gmail.com>
2019-07-07 20:34:37 +1200

    added madlibs

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a Celebrity")
+
+
+print("Roses are {}".format(color))
+print("{} are blue".format(plural_noun))
+print("I love {}".format(celebrity))
