ea8ff8d2394c8a67d6ff93a45c1e9896d08faa5e
jesse collier <jesse.collier97@gmail.com>
2019-07-07 18:06:31 +1200

    added input stuff

---
+++ input_from_users.py
@@ -0,0 +1,12 @@
+name = input("Enter Your Name: ")
+age = input("Enter your Age: ")
+print("Hello {} you are {} years old!".format(name, age))
+
+num1 = input("Enter a number")
+num2 = input("Enter another number")
+
+result = str(float(num1) + float(num2))
+print("your result is {}".format(result))
+# for i in [1,2,3]:
+#     print(i)
+
