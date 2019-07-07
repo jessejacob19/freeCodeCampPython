050f4c8f183a8c3a6c157756699f876cc5ed704d
jesse collier <jesse.collier97@gmail.com>
2019-07-07 16:29:58 +1200

    remembering format method

---
+++ variables.py
@@ -0,0 +1,7 @@
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + " , ")
+print("he was {} years old. ".format(character_age))
+print("He really Liked the name {}, ".format(character_name))
+print("but didn't like being {}.".format(character_age))
