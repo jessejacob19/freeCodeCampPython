f30ae095bb8866035f3675a78fc0ec753d982826
jesse collier <jesse.collier97@gmail.com>
2019-07-08 10:19:00 +1200

    basic for loop stuff

---
+++ for_loops.py
@@ -0,0 +1,10 @@
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Jim", "Karen", "Kevin"]
+for friend in friends:
+    print(friend)
+
+for index in range(3, 10):
+    print(index)
+
