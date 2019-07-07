8228154735246393dbfb54790dfe00a89a9aaa17
jesse collier <jesse.collier97@gmail.com>
2019-07-07 21:02:40 +1200

    added list functions

---
+++ py_tuples.py
@@ -0,0 +1,7 @@
+coordinates = (4, 5) #tuples
+#tuples are immutable cannot be changed
+
+print(coordinates[0])
+
+#coordinates[1] = 45 #does not work because it is immutable
+[(1, 2), (3, 4), (5, 6)] #tuples cannot be changed even in a list
