35ff7d2877e61b704e869cb87eb9ef54054ae09a
jesse collier <jesse.collier97@gmail.com>
2019-07-07 17:48:26 +1200

    added number methods

---
+++ working_with_numbers.py
@@ -0,0 +1,19 @@
+
+print(4)
+print(4.0192)
+
+num = 5
+print(str(num) + " my fav num") # anytime you want to print different operators together you need to cionvert to the same type
+
+#abs()
+#pow()
+#min()
+#max()
+#round() rounds up or down
+from math import *
+#floor() grabs the lowest whole number
+#ceil() rounds up
+#sqrt()
+
+
+
