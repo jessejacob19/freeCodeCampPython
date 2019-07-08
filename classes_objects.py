4ed8899c7986d0b60ae0fc765666b50ba5572a71
jesse collier <jesse.collier97@gmail.com>
2019-07-08 12:44:15 +1200

    basic classes

---
+++ classes_objects.py
@@ -0,0 +1,16 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Jesse", "Engineering", 0, True)
+
+print(student1.name)
+print(student2.gpa)
+
+
+'''
+ name is public
+ _name is protected
+ __name is private
+ 
+ but its all just convention doesent really matter
+'''
